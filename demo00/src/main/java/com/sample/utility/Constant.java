bb063c59f8093874965b5f1b675b105f4c3b055f
J01101 <jy102907@gmail.com>
2021-05-26 19:28:04 +0900

    encore_20210526
    encore_mainProject_20210526
    demo00_springMVC

---
+++ demo00/src/main/java/com/sample/utility/Constant.java
@@ -0,0 +1,11 @@
+package com.sample.utility;
+
+public class Constant {
+
+    public static final String driver="com.mysql.jdbc.Driver";
+    public static final String url="jdbc:mysql://127.0.0.1:3306/test_db";
+    public static final String user="root";
+    public static final String passwd="qwer1234";
+}
+    
+
---
+++ demo00/src/main/webapp/ssi/ssi_bbs.jsp
@@ -0,0 +1,7 @@
+<!-- <%@ page contentType="text/html; charset=UTF-8" %> 
+<%@ page import="java.util.*" %>
+<%@ page import="spring.utility.webtest.*" %>
+<%@ page import="spring.model.bbs.*" %>
+<%  
+    String root = request.getContextPath();
+%> -->
